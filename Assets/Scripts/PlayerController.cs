c09ab9f669d6e2aaf88cf2343d6170dccbb0d868
Mattia Colico <theworksgamer@gmail.com>
2020-03-14 00:52:01 +0100

    Animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,32 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+    public GameObject Character;
+    Animator Player;
     public Transform Body;
+    bool run = false;
     Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        Player = Character.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            run = true;
+            Player.SetBool("Run", run);
+        }
+        else
+        {
+            run = false;
+            Player.SetBool("Run", run);
+        }
         rb.AddForce(Body.forward * speed * Time.deltaTime * Input.GetAxis("Vertical"));
         Body.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0);
     }
---
+++ Assets/Scripts/Toiilet_Paper_Shooter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toiilet_Paper_Shooter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
