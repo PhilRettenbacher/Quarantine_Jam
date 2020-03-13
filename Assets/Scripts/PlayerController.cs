86342fe1e820ca43f37f3f7836ded2867c9e96f4
Phil <philipp.rettenbacher.student@htl-hallein.at>
2020-03-13 22:59:12 +0100

    Basic Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical"));
+        rb.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0);
+    }
+}
