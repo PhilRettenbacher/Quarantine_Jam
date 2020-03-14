2e34e2f7849b71ae89a186f4508aff59951dbb40
Phil <philipp.rettenbacher.student@htl-hallein.at>
2020-03-14 22:47:26 +0100

    points and collection

---
+++ Assets/Scripts/ShelfItemSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShelfItemSpawner : MonoBehaviour
+{
+    public Transform StartPoint;
+    public Transform EndPoint;
+    public float itemsPerUnit = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpawnItem()
+    {
+
+    }
+}
