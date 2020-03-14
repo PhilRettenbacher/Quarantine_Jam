65a46c06d8db8fb6bdf87cc1908e0cf69ce924c0
Mattia Colico <theworksgamer@gmail.com>
2020-03-14 01:40:45 +0100

    Test Market and Shooter

--- Assets/Scripts/Toiilet_Paper_Shooter.cs
+++ Assets/Scripts/Toiilet_Paper_Shooter.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Toiilet_Paper_Shooter : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject PlayerView;
+    //public Transform Hand;
+    public GameObject Bullet;
+    public float ammo;
+    public float distance;
     void Start()
     {
         
@@ -13,6 +19,20 @@ public class Toiilet_Paper_Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(PlayerView.transform.position, PlayerView.transform.forward * distance, Color.green);
+
+        if (Input.GetMouseButtonDown(0) && ammo >0)
+        {
+            ammo--;
+            GameObject B = Instantiate(Bullet, PlayerView.transform.position, Quaternion.identity);
+            Rigidbody bullet = B.GetComponent<Rigidbody>();
+            bullet.AddForce(PlayerView.transform.forward * 1200);
+            Destroy(B, 5f);
+
+        }
+
+
+
         
     }
 }
