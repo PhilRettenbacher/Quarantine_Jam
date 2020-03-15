18b2b8b8c6de9d5b7f8c81f1a65664d3c0409eaa
Mattia Colico <theworksgamer@gmail.com>
2020-03-15 20:26:21 +0100

    Light

---
+++ Assets/Scripts/Light_Settings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_Settings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Range,Intensity;
+    Light al;
+    void Start()
+    {
+        al = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        al.range = Range;
+        al.intensity = Intensity;
+        
+    }
+}
