f6003bb408754331dc48c203e2b70ed10fd42c80
Phil <philipp.rettenbacher.student@htl-hallein.at>
2020-03-15 21:28:29 +0100

    EndMenu and Highscores

--- Assets/Scripts/Checkout.cs
+++ Assets/Scripts/Checkout.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Checkout : MonoBehaviour
 {
     CartInventory inventory;
     public GameObject receiptUI;
+    public TMP_InputField highscoreInput;
+    Highscores highscore;
+    public int maxHighScores;
+    ReceiptEntry[] entries;
+    bool showHighScore = false;
+
+    private void Awake()
+    {
+        highscore = gameObject.GetComponent<Highscores>();
+    }
+
     public void GenerateReceipt()
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<CartInventory>();
         //var receipt = Instantiate(receiptUI);
         receiptUI.SetActive(true);
         ReceiptHandler handler = receiptUI.GetComponent<ReceiptHandler>();
-        for(int i = 0; i<ItemList.instance.itemPrefabs.Count;i++)
+        handler.buttonHighscore.SetActive(true);
+        handler.buttonMainMenu.SetActive(false);
+        handler.label.text = "Result";
+        for (int i = 0; i<ItemList.instance.itemPrefabs.Count;i++)
         {
             Item currItem = ItemList.instance.itemPrefabs[i].GetComponent<Item>();
             Time.timeScale = 0;
@@ -38,12 +54,89 @@ public class Checkout : MonoBehaviour
         entry1.itemAmount.text = inventory.score + ",-";
         entry1.itemAmount.fontStyle = TMPro.FontStyles.Bold;
     }
-    /*private void OnTriggerEnter(Collider other)
+
+    public void UploadHighscore()
     {
-        if(other.transform.root.tag=="Player")
+        if(!string.IsNullOrEmpty(highscoreInput.text))
         {
-            
-            GenerateReceipt();
+            highscore.AddNewHighscore(highscoreInput.text, inventory.score);
         }
-    }*/
+        InitShowHighscore();
+    }
+    public void InitShowHighscore()
+    {
+        receiptUI.SetActive(true);
+        highscore.DownloadHighscores();
+        ReceiptHandler handler = receiptUI.GetComponent<ReceiptHandler>();
+        handler.label.text = "Highscores";
+        handler.buttonHighscore.SetActive(false);
+        handler.buttonMainMenu.SetActive(true);
+        foreach(Transform child in handler.listObj.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        entries = new ReceiptEntry[maxHighScores];
+        for(int i = 0; i< entries.Length; i++)
+        {
+            var pos = Instantiate(handler.entryPrefab, handler.listObj.transform);
+            entries[i] = pos.GetComponent<ReceiptEntry>();
+        }
+        ShowHighScore();
+        StartCoroutine(RefreshHighScore());
+    }
+    public void ShowGameOver()
+    {
+        Time.timeScale = 0;
+        receiptUI.SetActive(true);
+        ReceiptHandler handler = receiptUI.GetComponent<ReceiptHandler>();
+        handler.buttonHighscore.SetActive(true);
+        handler.buttonMainMenu.SetActive(false);
+        handler.label.text = "Game Over";
+        handler.highScoreInput.SetActive(false);
+    }
+    IEnumerator RefreshHighScore()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        ShowHighScore();
+        yield return new WaitForSecondsRealtime(3);
+        ShowHighScore();
+        yield return new WaitForSecondsRealtime(10);
+        ShowHighScore();
+    }
+    public void ShowHighScore()
+    {
+        print("showhighscore");
+        if(highscore.highscoresList == null || highscore.highscoresList.Length == 0)
+        {
+            foreach (ReceiptEntry entry in entries)
+            {
+                entry.itemAmount.text = "";
+                entry.itemName.text = "Fetching ...";
+            }
+            return;
+        }
+        for(int i = 0; i<maxHighScores; i++)
+        {
+            if(i<highscore.highscoresList.Length)
+            {
+                entries[i].itemName.text = (i+1) + ". " + highscore.highscoresList[i].username;
+                entries[i].itemAmount.text = highscore.highscoresList[i].score.ToString();
+            }
+            else
+            {
+                entries[i].itemName.text = "";
+                entries[i].itemAmount.text = "";
+            }
+        }
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
 }
--- Assets/Scripts/ReceiptHandler.cs
+++ Assets/Scripts/ReceiptHandler.cs
@@ -8,4 +8,8 @@ public class ReceiptHandler : MonoBehaviour
     public GameObject listObj;
     public List<ReceiptEntry> entries = new List<ReceiptEntry>();
     public GameObject entryPrefab;
+    public TextMeshProUGUI label;
+    public GameObject highScoreInput;
+    public GameObject buttonHighscore;
+    public GameObject buttonMainMenu;
 }
