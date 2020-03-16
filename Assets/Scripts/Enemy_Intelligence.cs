1f819ba48c5fdd0121c4f3aa7db5d7470868fbd2
Mattia Colico <theworksgamer@gmail.com>
2020-03-16 22:13:27 +0100

    a

--- Assets/Scripts/Enemy_Intelligence.cs
+++ Assets/Scripts/Enemy_Intelligence.cs
@@ -9,123 +9,39 @@ public class Enemy_Intelligence : MonoBehaviour
 
     NavMeshAgent Enemy;
     Animator Weapon;
-    public Transform Sl,Sl2,Sf,Sr,Sr2, Obbiective;
+    int i;
     public bool Find = false,elab=false,elab2 = false;
-    public int n,i=0;
-    public GameObject Arm;
   //bool attack = false;
     public Transform[] Position = new Transform[10];
     
     void OnCollisionEnter(Collision collision)
     {
         //Debug.Log("Ti ha colpito:" + collision.gameObject.name);
-        if (collision.gameObject.tag == "Weapon")
+        if (collision.gameObject.tag == "Player")
         {
-            
-            Destroy(gameObject);
+
+            Rigidbody p = collision.gameObject.GetComponent<Rigidbody>();
+            p.AddForce(collision.transform.position * 200, ForceMode.Impulse);
 
         }
-        if(collision.gameObject.tag=="Takable" && gameObject.tag == "Weapon")
-        {
-            Find = !Find
-;        }
     }
 
 
     void Start()
     {
         Enemy = GetComponent<NavMeshAgent>();
-        Weapon = Arm.GetComponent<Animator>();
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        RaycastHit Obl, Obf, Obr;
-        if (Physics.Raycast(Sl.position,Sl.forward * 15, out Obl) && !Find)
+        
+        if (!Find)
         {
-            //Debug.Log("L colpisce :" + Obl.collider.gameObject.name);
-            Debug.DrawRay(Sl.position, Sl.forward * 15, Color.green);
-            if (Obl.collider.gameObject.tag == "Takable")
-            {
-                
-                Obbiective = Obl.collider.transform;
-                Find = true;
-            }
-        }
-        if (Physics.Raycast(Sl2.position, Sl2.forward * 15, out Obl) && !Find)
-        {
-            //Debug.Log("L colpisce :" + Obl.collider.gameObject.name);
-            Debug.DrawRay(Sl2.position, Sl2.forward * 15, Color.green);
-            if (Obl.collider.gameObject.tag == "Takable")
-            {
-
-                Obbiective = Obl.collider.transform;
-                Find = true;
-            }
-        }
-        if (Physics.Raycast(Sr.position, Sr.forward * 15, out Obr) && !Find)
-        {
-            //Debug.Log("R colpisce :" + Obl.collider.gameObject.name);
-            Debug.DrawRay(Sr.position, Sr.forward * 15, Color.green);
-            if (Obr.collider.gameObject.tag == "Takable")
-            {
-                
-                Obbiective = Obr.collider.transform;
-                Find = true;
-            }
-        }
-        if (Physics.Raycast(Sr2.position, Sr2.forward * 15, out Obr) && !Find)
-        {
-            //Debug.Log("R colpisce :" + Obl.collider.gameObject.name);
-            Debug.DrawRay(Sr2.position, Sr2.forward * 15, Color.green);
-            if (Obr.collider.gameObject.tag == "Takable")
-            {
-
-                Obbiective = Obr.collider.transform;
-                Find = true;
-            }
-        }
-        if (Physics.Raycast(Sf.position, Sf.forward * 15, out Obf))
-        {
-            //Debug.Log("F colpisce :" + Obl.collider.gameObject.name);
-            Debug.DrawRay(Sf.position, Sf.forward * 15, Color.green);
-            if (Obf.collider.gameObject.tag == "Takable" && !Find)
-            {
-                
-                Obbiective = Obf.collider.transform;
-                Find = true;
-            }
-            else if (Find && !elab)
-            {
-                if (Obf.collider.gameObject.tag != "Takable")
-                {
-                    StartCoroutine(WaitToFind(Obf,Obl,Obr));
-                }
-            }
-        }
-
-        if (Find && Obbiective != null)
-        {
-            i = 0;
-            transform.LookAt(Obbiective);
-            Enemy.SetDestination(Obbiective.position);
-            /*if (Enemy.remainingDistance < 3 && !attack)
-            {
-                attack = true;
-                string temp = gameObject.tag;
-                gameObject.tag = "Weapon";
-                Weapon.SetBool("Attack", attack);
-                StartCoroutine(WaitAttack(temp));
-            }*/
-        }
-
-        else if (Find && Obbiective == null)
-            Find = false;
-
-        else if (!Find)
-        {
+            transform.LookAt(Position[i].position);
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y - 180, transform.rotation.z);
             Enemy.SetDestination(Position[i].position);
             if (Enemy.remainingDistance < 2f)
             {
@@ -156,74 +72,5 @@ public class Enemy_Intelligence : MonoBehaviour
 
     }*/
 
-    IEnumerator WaitToFind(RaycastHit Obf, RaycastHit Obl, RaycastHit Obr)
-    {
-        
-        if(!elab)
-        {
-            elab = true;
-            n = 0;
-            while (n<500 && Obbiective != null)
-            {
-                
-                yield return new WaitForSeconds(0.01f);
-                if (Physics.Raycast(Sf.position, Sf.forward * 15, out Obf))
-                {
-                    Debug.DrawRay(Sf.position, Sf.forward * 15, Color.green);
-                    if (Obf.collider.gameObject.tag == "Takable")
-                    {
-                        break;
-                    }
-                    
-                }
-                if (Physics.Raycast(Sl.position, Sl.forward * 15, out Obl))
-                {
-                    Debug.DrawRay(Sl.position, Sl.forward * 15, Color.green);
-                    if (Obf.collider.gameObject.tag == "Takable")
-                    {
-                        break;
-                    }
-                    
-                }
-                if (Physics.Raycast(Sl2.position, Sl2.forward * 15, out Obl))
-                {
-                    Debug.DrawRay(Sl2.position, Sl2.forward * 15, Color.green);
-                    if (Obf.collider.gameObject.tag == "Takable")
-                    {
-                        break;
-                    }
-                    
-                }
-                if (Physics.Raycast(Sr.position, Sr.forward * 15, out Obr))
-                {
-                    Debug.DrawRay(Sr.position, Sr.forward * 15, Color.green);
-                    if (Obf.collider.gameObject.tag == "Takable")
-                    {
-                        break;
-                    }
-                    
-                }
-                if (Physics.Raycast(Sr2.position, Sr2.forward * 15, out Obr))
-                {
-                    Debug.DrawRay(Sr2.position, Sr2.forward * 15, Color.green);
-                    if (Obf.collider.gameObject.tag == "Takable")
-                    {
-                        break;
-                    }
-                    
-                }
-
-                
-                if (Find == true && n == 499 || Obbiective == null)
-                {
-                    Find = false;
-                    Obbiective = null;
-                }
-                    
-                n++;
-            }
-
-            elab = false;
-        }
-    }
+    
 }
